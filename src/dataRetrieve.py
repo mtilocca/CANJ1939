53b3cd01145edd58f1ce4356347e20af85babad5
mtilocca <mario.tilocca@gmail.com>
2024-03-08 12:01:58 +0100

    Improve data retrieve with config files

--- src/dataRetrieve.py
+++ src/dataRetrieve.py
@@ -1,5 +1,31 @@
 import numpy as np 
 import pandas as pd 
+import json
+
+class dataRetrieve:
+    def __init__(self, dtype_path='dftype.json', columns_path='columns.json', data_path='dataToCan.txt'):
+        with open(dtype_path, 'r') as f:
+            dftype = json.load(f)
+        with open(columns_path, 'r') as f:
+            columns = json.load(f)
+        
+        # Convert string types to actual numpy dtypes
+        self.dftype = {key: np.dtype(value) for key, value in dftype.items()}
+
+        # Ensure column indices are integers
+        self.dict = {int(key): value for key, value in columns.items()}
+
+        self.df1 = pd.read_csv(data_path, header=None)
+        self.df1 = self.df1.rename(columns=self.dict)
+        self.df1 = self.df1.astype(self.dftype)
+
+    def completeData(self):
+        return self.df1
+
+    def retrieve(self, col):
+        arr = self.df1[col].copy()
+        return arr
+
 
 
 # EXPLANATION OF DATASTRUCT CONTENT 
@@ -36,70 +62,3 @@ each column represents:
 18] traveled distance 
 
 '''
-
-
-
-class dataRetrieve:
-
-    def __init__(self):
-
-        self.dftype = {'alphaFR': np.float16,
-                            'alphaFL': np.float16,
-                            'Speed': np.float16,
-                            'Rpm': np.float16,
-                            'frame': int,
-                            'GearRatio': np.float16,
-                            'alphaAxle': np.float16,
-                            'Ofl': np.float16,
-                            'Ofr': np.float16,
-                            'Orl': np.float16,
-                            'Orr': np.float16,
-                            'lat': np.float32,
-                            'lon': np.float32,
-                            'h': np.float16, 
-                            'pitch': np.float16, 
-                            'yaw': np.float16, 
-                            'roll': np.float16}
-        
-        self.dict = { 0: 'alphaFR',
-                      1 : 'alphaFL',
-                      2 : 'Speed',
-                      3 : 'Rpm',
-                      4 : 'frame',
-                      5 : 'GearRatio',
-                      6 : 'alphaAxle',
-                      7 : 'Ofl',
-                      8 : 'Ofr',
-                      9 : 'Orl',
-                      10 : 'Orr',
-                      11 : 'lat', 
-                      12 : 'lon',
-                      13 : 'h',
-                      14 : 'pitch',
-                      15 : 'yaw', 
-                      16 : 'roll',
-                      17 : 'travelD'}
-
-        self.df1 = pd.read_csv(r'C:\Users\M. Tilocca\Documents\My Games\FarmingSimulator2019\dataToCan.txt', header = None ) #error
-        self.df1 = self.df1.rename(columns=self.dict)
-        self.df1 = self.df1.astype(self.dftype)
-
-    #def info(self): # used to keep track of number of rows --> to be used for the recorded data mainly 
-        #info = self.dataStruct.shape 
-
-        #info = list(info)
-        #rowN = info[0]
-
-        #return rowN
-        
-
-    def completeData(self):
-        return self.df1
-
-    def retrieve(self, col):
-        arr = self.df1[col].copy()
-        
-        return arr
-
-
-
